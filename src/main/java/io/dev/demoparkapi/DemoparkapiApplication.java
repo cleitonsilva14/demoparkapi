7a7be147d6155afee482194bc84a432914f3a1a9
cleitonsilva14 <cleiton18ads@gmail.com>
2025-01-29 07:39:08 -0300

    Primeiro commit, subindo projeto

---
+++ src/main/java/io/dev/demoparkapi/DemoparkapiApplication.java
@@ -0,0 +1,13 @@
+package io.dev.demoparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoparkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoparkapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dev/demoparkapi/DemoparkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dev.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoparkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
