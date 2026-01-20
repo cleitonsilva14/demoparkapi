f08c9d8d6fc527dac67783ec62a955b796daaa60
cleitonsilva14 <cleiton18ads@gmail.com>
2026-01-20 11:05:45 -0300

    Adicionando bkp banco

---
+++ bkp.db.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE `demo_park_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(1, NULL, NULL, NULL, NULL, '112233', 'ROLE_CLIENTE', 'clockiiin@spring.io.com');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(3, NULL, NULL, NULL, NULL, 'clock123', 'ROLE_CLIENTE', 'clock@spring.io.com');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(4, NULL, NULL, NULL, NULL, '1234', 'ROLE_CLIENTE', 'clould@spring.io.com');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(5, NULL, NULL, NULL, NULL, '123456', 'ROLE_CLIENTE', 'green@spring.io.com');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(7, NULL, NULL, NULL, NULL, '123456', 'ROLE_CLIENTE', 'lightgreen@spring.io');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(9, NULL, NULL, NULL, NULL, '123456', 'ROLE_CLIENTE', 'blue@spring.io');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(10, NULL, NULL, NULL, NULL, '123456', 'ROLE_CLIENTE', 'white@spring.com');
+INSERT INTO demo_park_db.usuarios
+(id, criado_por, data_criacao, data_modificacao, modificado_por, password, `role`, username)
+VALUES(14, NULL, NULL, NULL, NULL, '112233', 'ROLE_CLIENTE', 'green@spring.io');
