2bc3b9e5265748fa32cbf2fce02da3c717bc3be2
cleitonsilva14 <cleiton18ads@gmail.com>
2026-03-21 10:53:37 -0300

    Ajuste /

--- src/main/java/io/dev/demoparkapi/config/SpringSecurityConfig.java
+++ src/main/java/io/dev/demoparkapi/config/SpringSecurityConfig.java
@@ -26,7 +26,7 @@ public class SpringSecurityConfig {
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/usuarios").permitAll()
                                 .anyRequest().authenticated()
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
